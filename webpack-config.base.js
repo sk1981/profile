11a24d6f0a5b951107196401a2eb9c5585c66577
sk1981 <sinavasi@gmail.com>
2016-07-03 23:38:10 +0530

    Basic structure with dev build files

---
+++ webpack-config.base.js
@@ -0,0 +1,53 @@
+var webpack = require('webpack');
+var path = require('path');
+var autoprefixer = require('autoprefixer');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: ['./src/index', './style/main.scss', './index.jade'],
+    vendor: ['d3']
+  },
+  output: {
+    path: path.join(__dirname, 'build'),
+    libraryTarget: 'umd',
+    filename: '[name]-bundle.js'
+    // publicPath: 'http://localhost:8080/build'
+  },
+  module: {
+    loaders: [
+      {
+        test: /.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015'],
+          plugins: ['transform-es2015-function-name']
+        }
+      },
+      {
+        test: /\.scss?$/,
+        exclude: /node_modules/,
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader!!postcss-loader!sass-loader")
+      },
+      {
+        test: /.jade?$/,
+        loader: 'jade-loader',
+        exclude: /node_modules/,
+        options: {}
+      }
+    ]
+  },
+  postcss: [
+    autoprefixer({browsers: ['last 3 versions']})
+  ],
+  plugins: [
+    new ExtractTextPlugin("[name]-styles.css"),
+    new HtmlWebpackPlugin({
+      template: './index.jade',
+      minify: false
+    })
+  ]
+};
+
---
+++ webpack-config.dev.js
@@ -0,0 +1,13 @@
+'use strict';
+var webpack = require('webpack');
+const merge = require('webpack-merge');
+const webpackCommon = require('./webpack-config.base');
+
+module.exports = merge(webpackCommon, {
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor'],
+      minChunks: Infinity
+    })
+  ]
+});
