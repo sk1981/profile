50d90479dcd8fb677e02160ba0191491cea06464
sk1981 <sinavasi@gmail.com>
2016-07-20 04:49:05 +0530

    Added star animation to header background

--- src/background/BackgroundCanvasSetup.js
+++ src/background/BackgroundCanvasSetup.js
@@ -1,21 +1,32 @@
 import {addResizeListener, getSize} from '../events/windowResizeManager';
 import StarFallAnimator from './StarFallAnimator';
 
+// Store it to avoid re-querying it evenu time
+const headerElement = document.getElementsByTagName('header')[0];
+
+function getHeaderSize() {
+  const boundingBox = headerElement.getBoundingClientRect();
+  return {
+    height: boundingBox.height || boundingBox.bottom - boundingBox.top,
+    width: boundingBox.width || boundingBox.right - boundingBox.left
+  }
+}
+
 /**
  * Sets up Canavs to display BG image
  */
 export function setUpBackground() {
   const canvas = document.getElementById('canvasBg');
   if(canvas !== undefined) {
-    const size = getSize();
+    const size = getHeaderSize();
     canvas.height = size.height;
     canvas.width = size.width;
     const canvasContext = canvas.getContext('2d');
     canvasContext.fillStyle = 'white';
-    const animator = new StarFallAnimator(canvasContext, size.height, size.width);
+    let animator = new StarFallAnimator(canvasContext, size.height, size.width);
     animator.updatePos();
-    //TODO : full re-draw on size change
-    addResizeListener((width, height) => {
+    addResizeListener(() => {
+      const size = getHeaderSize();
       canvas.height = height;
       canvas.width = width;
       canvasContext.fillStyle = 'white';
